54d8036ed90afd235ed43772e12f52c20808d0f0
kzusg <94691499+KAZUKI-0130@users.noreply.github.com>
2023-06-19 20:54:21 +0900

    Update member_join.js

--- events/client/member_join.js
+++ events/client/member_join.js
@@ -2,16 +2,12 @@ const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
 
-client.once('ready', () => {
-    console.log('Ready!'); // 起動した時に"Ready!"とコンソールに出力する
-});
-
 client.on("guildMemberAdd", member => {
-    if (member.guild.id !== "利用するサーバーId") return; // 指定のサーバー以外では動作しないようにする
-    member.guild.channels.cache.get("送信するチャンネルId").send(${member.user}が参加しました！);
+    if (member.guild.id !== "934001300553740318") return; // 指定のサーバー以外では動作しないようにする
+    member.guild.channels.cache.get("1120320289956446279").send(${member.user}が参加しました！);
 });
 
 client.on("guildMemberRemove", member => {
-    if (member.guild.id !== "利用するサーバーId") return; // 指定のサーバー以外では動作しないようにする
-    member.guild.channels.cache.get("送信するチャンネルId").send(${member.user.tag}が退出しました。);
+    if (member.guild.id !== "934001300553740318") return; // 指定のサーバー以外では動作しないようにする
+    member.guild.channels.cache.get("1120320289956446279").send(${member.user.tag}が退出しました。);
 });
